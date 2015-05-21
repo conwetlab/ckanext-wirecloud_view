33adfb3a677d26f4104365c9342d1fdebadeb81f
Borja Illescas Medina <billescas@conwet.com>
2015-05-21 13:47:24 +0200

    get_workspaces function done

--- ckanext/wirecloudview/plugin.py
+++ ckanext/wirecloudview/plugin.py
@@ -2,18 +2,38 @@ import logging
 import ckan.plugins as p
 from ckan.plugins.toolkit import Invalid
 import re
+from requests_oauthlib import OAuth2Session
+import pylons.config as config
+import json
 
 log = logging.getLogger(__name__)
+wcURL = config.get('ckan.wirecloud_view.url', False)
+client_id = config.get('ckan.oauth2.client_id', False)
 
 def is_url(url):
     if not re.match('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', url):
         raise Invalid('This field must contain a valid url.')
     return url
 
+def get_workspaces():
+    log.debug("GET WORKSPACES()")   
+    
+    token = p.toolkit.c.usertoken
+    oauth = OAuth2Session(client_id, token=token)
+    
+    response = oauth.get( wcURL + "\\api\workspaces" + '?access_token=%s' % token['access_token'])
+    log.debug(response.text)
+    workspaces = json.loads(response.text)
+
+    #log.debug(workspaces)
+
+    return workspaces
+
 class WirecloudView(p.SingletonPlugin, p.toolkit.DefaultDatasetForm):
 
     p.implements(p.IConfigurer)
     p.implements(p.IResourceView)
+    p.implements(p.ITemplateHelpers)    
 
     def update_config(self, config):
         p.toolkit.add_template_directory(config, 'templates')
@@ -33,7 +53,12 @@ class WirecloudView(p.SingletonPlugin, p.toolkit.DefaultDatasetForm):
         return False #If someone adds this view to default_views to avoid an empty iframe
 
     def view_template(self, context, data_dict):
+        log.debug("VIEW TEMPLATE")
+        get_workspaces()
         return 'wirecloud_view.html'
 
     def form_template(self, context, data_dict):        
         return 'wirecloud_form.html'
+
+    def get_helpers(self):
+        return {'get_workspaces': get_workspaces}
