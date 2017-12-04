ef32f4815c6579c363b4a4f5b239e89285b112f9
Álvaro Arranz <aarranz@ficodes.com>
2017-12-04 18:14:20 +0100

    Initial tests

--- ckanext/wirecloudview/tests/test_plugin.py
+++ ckanext/wirecloudview/tests/test_plugin.py
@@ -1,5 +1,15 @@
 """Tests for plugin.py."""
 import ckanext.wirecloudview.plugin as plugin
 
-def test_plugin():
-    pass
+from mock import MagicMock, patch
+
+
+class DataRequestPluginTest(unittest.TestCase):
+
+    def test_process_dashboardid_should_strip(self):
+
+        self.assertEqual(plugin.process_dashboardid(self, "  owner/name ", context), "onwer/name")
+
+    def test_process_dashboardid_should_leave_untouched_valid_dashboard_ids(self):
+
+        self.assertEqual(plugin.process_dashboardid(self, "owner/name", context), "onwer/name")
