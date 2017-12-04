1f1b6c2c84c9650f897f823e30c23f1ee72fa130
Álvaro Arranz <aarranz@ficodes.com>
2017-12-04 16:47:06 +0100

    Bump version

--- ckanext/wirecloudview/__init__.py
+++ ckanext/wirecloudview/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2011-2016 CoNWeT Lab., Universidad Politécnica de Madrid
+# Copyright (c) 2011-2017 CoNWeT Lab., Universidad Politécnica de Madrid
 
 # This file is part of Wirecloud.
 
@@ -18,4 +18,4 @@
 # along with Wirecloud.  If not, see <http://www.gnu.org/licenses/>.
 
 __version_info__ = (1, 0, 0)
-__version__ = '.'.join(map(str, __version_info__)) + 'b2'
+__version__ = '.'.join(map(str, __version_info__))
