21007fe94d35121a450147439d42c557e4ccb234
Álvaro Arranz <aarranz@ficodes.com>
2018-02-09 13:49:47 +0100

    Fix postgres problem on Travis using a newer version of psycopg2 instead of using postgres 9.1

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -12,6 +12,9 @@ git clone https://github.com/ckan/ckan
 cd ckan
 git checkout ckan-$CKANVERSION
 python setup.py develop
+
+sed -i "s|psycopg2==2.4.5|psycopg2==2.7.1|g" requirements.txt
+
 pip install -r requirements.txt --allow-all-external
 pip install -r dev-requirements.txt --allow-all-external
 cd -
